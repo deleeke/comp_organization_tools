71da99203e7dc817c39ac025d4fbc4be9051c2af
deleeke <deleeke@gmail.com>
2017-03-29 21:24:11 -0400

    fixed bug in decimal to hex conversion (leading 0 for positive numbers was throwing off wordsize) and added a couple simple tests

--- comp_organization_tools/__init__.py
+++ comp_organization_tools/__init__.py
@@ -38,13 +38,28 @@ bin_hex_dict = { '0000' : '0',
                  '1111' : 'F'}
 
 
-def binToDec(num):
+def binToDec(num, verbose=True):
+    '''Converts two's complement binary integer to decimal (base ten). 
+
+    Interperets string of 0's and 1's or integer consisting of 0's and 1's as a two's complement integer and converts to base 10.
+
+    Args:
+        num    (string or int): Binary number to be converted -- no spaces allowed
+    
+    Optional Args:
+        verbose          (bool): By default, set to true. Prints out all the steps taken.
+
+    Returns:
+        integer value of binary string '''
+
     num_str = str(num)
-    print ('Convert %s to decimal' %num_str)
+    if verbose:
+        print ('Convert %s to decimal' %num_str)
     result = 0
     isNeg = False
     if num_str[0] == '1':
-        print ("If negative, take two's complement")
+        if verbose:
+            print ("If negative, take two's complement")
         isNeg = True
         num_str = twosComplement(num_str)
 
@@ -55,8 +70,9 @@ def binToDec(num):
         result = -result
     return result
 
-def twosComplement(num):
-    print ("Two's complement of %s" %num)
+def twosComplement(num, verbose=True):
+    if verbose:
+        print ("Two's complement of %s" %num)
     num_str = str(num)
     return_str1 = ''
     return_str2 = ''
@@ -66,8 +82,9 @@ def twosComplement(num):
             return_str1 += '1'
         else:
             return_str1 += '0'
-    print ('flipped bits')
-    print ("%s --> %s" %(num_str, return_str1))
+    if verbose:
+        print ('flipped bits')
+        print ("%s --> %s" %(num_str, return_str1))
     # add 1
     overflow = True
     for i in range(1, len(return_str1) + 1):
@@ -96,14 +113,28 @@ def bitsToEncode(N):
     '''Returns number of bits it takes to encode N possible states'''
     return int(math.ceil(math.log(N,2)))
 
-def decToBin(num, wordsize=0):
-    '''Converts to two's complement binary integer, if no wordsize provided, uses as many bits as necessary'''
+def decToBin(num, wordsize=0, verbose=True):
+    '''Converts base 10 integer to binary.
+
+    Binary representation uses two's complment to signify negative values
+
+    Args:
+        num               (int): Number to be converted
+    
+    Optional Args:
+        wordsize          (int): By default, this will size the binary string to whatever will hold the number. If a insufficient wordsize is specified, overflow will occur -- but this is by design.
+        verbose          (bool): By default, set to true. Prints out all the steps taken.
+
+    Returns:
+        string representing two's complement binary value.
+    '''
     if wordsize == 0:
         if num > 0:
             wordsize = int(math.log(num, 2)) +1
         else:
             wordsize = 4
-    print ("Convert decimal %s to binary" %num)
+    if verbose:
+        print ("Convert decimal %s to binary" %num)
     isNeg = False
     if num < 0:
         num = -num
@@ -123,35 +154,67 @@ def decToBin(num, wordsize=0):
         return_str = twosComplement(return_str)
     return return_str
 
-def hexToBinary(hex_num, seperator=''):
-    '''Assumes two's complement representation'''
+def hexToBinary(hex_num, seperator='', verbose=True):
+    '''Converts hexadecimal to binary.
+
+    Interperets string of characters from {0 ... 9} and {A ... F} as a hexadecimal number 
+    and returns its value in two's complement binary.
+
+    Args:
+        hex_str        (string): Hex number to be converted -- no spaces allowed, no leading 0x
+    
+    Optional Args:
+        verbose          (bool): By default, set to true. Prints out all the steps taken.
+
+    Returns:
+        string representing two's complement binary value
+    '''
+    hex_num = str(hex_num)
     return_str = ''
     pretty_str = ''
     for i in range(len(hex_num)):
         pretty_str += hex_bin_dict[hex_num[i]] + " "
         return_str += hex_bin_dict[hex_num[i]] + seperator
-    print ('hex 0x%s --> %s in binary' %(hex_num, pretty_str))
+    if verbose:    
+        print ('hex 0x%s --> %s in binary' %(hex_num, pretty_str))
     return return_str
 
-def hexToDec(hex_num):
-    result = binToDec(hexToBinary(hex_num))
-    print ("hex 0x%s --> %s in decimal" %(hex_num, result))
+def hexToDec(hex_num, verbose=True):
+    result = binToDec(hexToBinary(hex_num, verbose=verbose), verbose=verbose)
+    if verbose:
+        print ("hex 0x%s --> %s in decimal" %(hex_num, result))
     return result
 
-def decToHex(dec_int, wordsize=64):
-    return binToHex(decToBin(dec_int), wordsize)
+def decToHex(dec_int, verbose=True):
+    return binToHex(decToBin(dec_int, wordsize=63, verbose=verbose), verbose=verbose)
 
-def binToHex(bin_str, wordsize=64):
-    bin_str = decToBin(binToDec(bin_str), wordsize)
-    print ("Convert binary %s to hex" %bin_str)
+def binToHex(bin_str, wordsize=64, verbose=True):
+    '''Converts binary to hexadecimal.
+
+    Interperets string of 0's and 1's or integer consisting of 0's and 1's as a two's complement integer.
+
+    Args:
+        bin_str (string or int): Binary number to be converted -- no spaces allowed
+    
+    Optional Args:
+        wordsize          (int): Number of bits that hexadecimal number will represent. Must be multiple of 4.
+        verbose          (bool): By default, set to true. Prints out all the steps taken.
+
+    Returns:
+        string representing two's complement hexadecimal value.
+    '''
+    bin_str = str(bin_str)
+   # bin_str = decToBin(binToDec(bin_str, verbose=verbose), wordsize)
+    if verbose:
+        print ("Convert binary %s to hex" %bin_str)
     hex_num = ''
     i = 0
-    while(i+4 <= wordsize):
+    while(i+3 <= wordsize):
         nibble = ''
         for k in range(4):
             nibble += bin_str[i+k]
         hex_num += bin_hex_dict[nibble]
         i += 4
-    print ('%s --> 0x%s in hex' %(bin_str, hex_num))
+    if verbose:
+        print ('%s --> 0x%s in hex' %(bin_str, hex_num))
     return hex_num
-
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,11 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
+from comp_organization_tools import *
 
 def test_success():
     assert True
+
+def test_decToBin():
+    for k in range(1000):
+        assert (binToDec(decToBin(k)) == k)
+def test_hexToDec():
+    for k in range(1, 1000):
+        assert (hexToDec(decToHex(k, verbose=False), verbose=False) == k)
